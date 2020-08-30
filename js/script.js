84e7614f676b2b9833b5b780d7acc0babd399d55
hamsoace <hamsoace@gmail.com>
2020-08-31 02:43:03 +0300

    added click functionality in jquery

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#design").click(function() {
+        $("#design-p").toggle();
+    });
+
+    $("#development").click(function() {
+        $("#development-p").toggle();
+    });
+
+    $("#product").click(function() {
+        $("#product-p").toggle();
+    });
+
+    $("p").css("text-align", "center");
+
+    $("h5").css("text-align", "center");
+
+    $("h5").css("font-weight", "bold");
+
+    $("h4").css("text-align", "center");
+
+
+    
+});
