0576c92def5e8733064dd0afdbc77e2b8b49efb7
hamsoace <hamsoace@gmail.com>
2020-08-31 02:44:32 +0300

    added more functionality in jquery

--- js/script.js
+++ js/script.js
@@ -19,6 +19,56 @@ $(document).ready(function() {
 
     $("h4").css("text-align", "center");
 
+    $("#wrap1").mouseover(function() {
+        $("#overlay1").show();
+    }).mouseout(function() {
+        $("#overlay1").hide();
+    });
 
-    
+
+    $("#wrap2").mouseover(function() {
+        $("#overlay2").show();
+    }).mouseout(function() {
+        $("#overlay2").hide();
+    });
+
+    $("#wrap3").mouseover(function() {
+        $("#overlay3").show();
+    }).mouseout(function() {
+        $("#overlay3").hide();
+    });
+
+    $("#wrap4").mouseover(function() {
+        $("#overlay4").show();
+    }).mouseout(function() {
+        $("#overlay4").hide();
+    });
+
+    $("#wrap5").mouseover(function() {
+        $("#overlay5").show();
+    }).mouseout(function() {
+        $("#overlay5").hide();
+    });
+
+    $("#wrap6").mouseover(function() {
+        $("#overlay6").show();
+    }).mouseout(function() {
+        $("#overlay6").hide();
+    });
+
+    $("#wrap7").mouseover(function() {
+        $("#overlay7").show();
+    }).mouseout(function() {
+        $("#overlay7").hide();
+    });
+
+    $("#wrap8").mouseover(function() {
+        $("#overlay8").show();
+    }).mouseout(function() {
+        $("#overlay8").hide();
+    });
+
+
+    $(".project").css("margin-top", "30%");
+
 });
